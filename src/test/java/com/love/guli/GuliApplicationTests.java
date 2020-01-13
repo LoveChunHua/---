e46a6d0097c5592b1f16f84f12d80d82353f2053
sunkai <18765861848@163.com>
2020-01-13 22:47:59 +0800

    readme.md

---
+++ src/test/java/com/love/guli/GuliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.guli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
